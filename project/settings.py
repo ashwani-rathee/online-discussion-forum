35378540d531f5e8b20900178b3093ab5e6936ce
Ashwani Rathee <ab669522@gmail.com>
2021-08-18 21:42:47 +0530

    debugger on

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mj16$30&s%(@9(k1-zuo2=s25p#i$matld^(6eje4co+@llx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['discussionforumwost.herokuapp.com','127.0.0.1']
 
