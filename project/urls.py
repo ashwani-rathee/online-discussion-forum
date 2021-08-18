2af47b4c3275645938d0e854f3535d31365c6f22
Ashwani Rathee <ab669522@gmail.com>
2021-08-18 21:51:03 +0530

    main urls fix

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls")),
+    path('', include('main.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('account/', include('register.urls')),
     path('hitcount/', include('hitcount.urls', namespace='hitcount')),
